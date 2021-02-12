52f3533e208d79a50b133f8b9c666d81cda5be66
jochem.dekruijf <jochem.dekruijf@student.hu.nl>
2021-02-12 11:02:45 +0100

    Test map aangemaakt

---
+++ test/com/java/practicums/practicum2B/VoetbalclubTest.java
@@ -0,0 +1,12 @@
+package com.java.practicums.practicum2B;
+
+import org.junit.jupiter.api.Test;
+
+class VoetbalclubTest {
+
+    @Test
+    void voetbalClubnaamTest() {
+        Voetbalclub club = new Voetbalclub("", 2, 3, 2);
+        System.out.println("test");
+    }
+}
