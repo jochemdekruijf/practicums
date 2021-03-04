40c640e4c539f68115a598e81c3aa619869d657b
jochem.dekruijf <jochem.dekruijf@student.hu.nl>
2021-03-04 20:18:56 +0100

    practicum6A in 6B gezet voor practicum6B

---
+++ src/com/java/practicums/practicum6B/Persoon.java
@@ -0,0 +1,65 @@
+package com.java.practicums.practicum6B;
+
+import java.util.ArrayList;
+
+public class Persoon {
+    private String naam;
+    private double budget;
+    private ArrayList<Game> games = new ArrayList<Game>();
+
+    public Persoon(String naam, double budget) {
+        this.naam = naam;
+        this.budget = budget;
+    }
+
+    public boolean koop(Game g) {
+        if (this.budget >= g.huidigeWaarde()) {
+            for (Game game : games) {
+                if (game.equals(g)) {
+                    return false;
+                }
+            }
+            games.add(g);
+            budget -= g.huidigeWaarde();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean verkoop(Game g, Persoon koper) {
+        if (games.contains(g)) {
+            if (koper.koop(g)) {
+                budget += g.huidigeWaarde();
+                games.remove(g);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    @Override
+    public String toString() {
+        String string = naam + " heeft een budget van €" + String.format("%.2f", budget) + " en bezit de volgende games:";
+        for (Game g : games) {
+            string += "\n" + g;
+        }
+        return string;
+
+    }
+}
